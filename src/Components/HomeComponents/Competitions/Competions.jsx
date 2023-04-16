2040a4c5b93092465f7669f3f6f08e791957b9c4
Ahsan <omerjee.ahsan@gmail.com>
2023-04-17 02:19:52 +0500

    competitions cards done

--- src/Components/HomeComponents/Competitions/Competions.jsx
+++ src/Components/HomeComponents/Competitions/Competions.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import "./Competions.css"
+import ComputerIcon from '@mui/icons-material/Computer';
+import { purple } from '@mui/material/colors';
 
 const Competions = () => {
 
@@ -7,35 +9,42 @@ const Competions = () => {
     {
       id:1,
       heading:'Speed Programming',
-      description:'In this competition, participants are given a programming task that they must complete as quickly as possible. The faster they can write the code and get it to run successfully, the higher their score. This competition is perfect for those who are skilled at programming and can work quickly under pressure.',
-      img:'/Images/speed_prog.jpg'
+      description:'In this competition, participants are given a programming task that they must complete as quickly as possible. The faster they can write the code and get it to run successfully, the higher their score.',
+      img:<ComputerIcon sx={{ fontSize: 70,color: purple[500] }} />
     },
     {
       id:1,
       heading:'Speed Debugging',
-      description:'In this competition, participants are given a piece of code that contains errors, and they have to find and fix them as quickly as possible. The faster they can identify and correct the errors, the higher their score. This competition is perfect for those who have a knack for finding bugs and enjoy the challenge of solving problems quickly. ',
-      img:'/Images/speed_debug.jpg'
+      description:'In this competition, participants are given a piece of code that contains errors, and they have to find and fix them as quickly as possible. The faster they can identify and correct the errors, the higher their score.',
+      img:<ComputerIcon sx={{ fontSize: 70,color: purple[500] }} />
     },
     {
       id:1,
       heading:'UI/UX Design ',
-      description:'In this competition, participants are given a design challenge where they have to create a user interface that is both visually appealing and easy to use. This competition is perfect for those who have an eye for design and understand the importance of creating a user-friendly experience. ',
-      img:'/Images/uiux.jpeg'
+      description:'In this competition, participants have to create a user interface that is both visually appealing and easy to use. This competition is perfect for those who have an eye for design.',
+      img:<ComputerIcon sx={{ fontSize: 70, color: purple[500] }} />
     },
   ]
 
 return (
    <>
    <div className='comp_main'>
-      <h2 className="mainheading">Competitions Disciplines</h2>
+
+      
 
      <main>
+    <div className="main_card">
+      <h2>Koderz Competitons</h2>
+      <p>Here are some of our Competitions Disciplines</p>
+    </div>
    {
      data.map((h=>{
        return (
          
          <div class = "card">
-    <img src={h.img} alt=""  />
+          <div className="icon">
+        {h.img}
+          </div>
     
     <div class="card-content">
       <h2>
@@ -44,10 +53,8 @@ return (
       <p>
         {h.description}
       </p>
-      <a href="#" class="button">
-        Find out more 
-      </a>
     </div>
+
   </div>
       )
     }))
