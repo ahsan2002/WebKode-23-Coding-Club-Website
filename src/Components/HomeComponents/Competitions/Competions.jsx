3b03570d086c7733b3321e46119162477353631b
Ahsan <omerjee.ahsan@gmail.com>
2023-04-19 05:10:37 +0500

    reg form in progress

--- src/Components/HomeComponents/Competitions/Competions.jsx
+++ src/Components/HomeComponents/Competitions/Competions.jsx
@@ -11,21 +11,21 @@ const Competions = () => {
       id: 1,
       heading: 'Speed Programming',
       description: 'In this competition, participants are given a programming task that they must complete as quickly as possible. The faster they can write the code and get it to run successfully, the higher their score.',
-      img:<ComputerIcon sx={{ fontSize: 70,color: purple[500] }} />
+      img:<ComputerIcon sx={{ fontSize: 50,color: purple[500] }} />
       // img:<MdComputer />
     },
     {
       id: 1,
       heading: 'Speed Debugging',
       description: 'In this competition, participants are given a piece of code that contains errors, and they have to find and fix them as quickly as possible. The faster they can identify and correct the errors, the higher their score.',
-      img:<ComputerIcon sx={{ fontSize: 70,color: purple[500] }} />
+      img:<ComputerIcon sx={{ fontSize: 50,color: purple[500] }} />
       // img:<MdComputer />
     },
     {
       id: 1,
       heading: 'UI/UX Design ',
       description: 'In this competition, participants have to create a user interface that is both visually appealing and easy to use. This competition is perfect for those who have an eye for design.',
-      img:<ComputerIcon sx={{ fontSize: 70, color: purple[500] }} />
+      img:<ComputerIcon sx={{ fontSize: 50, color: purple[500] }} />
       // img:<MdComputer />
     },
   ]
--- src/Components/HomeComponents/MainPage/Main.jsx
+++ src/Components/HomeComponents/MainPage/Main.jsx
@@ -9,7 +9,7 @@ const Main = () => {
 
         <div className="intro">
           <h1>Koderz Club</h1>
-          <p>Official Community of The Department of <br /> Computer Science & Information Technology, NEDUET<br /> <br /> We're always looking for new members to join our community, <br /> whether you're an experienced programmer or just starting out.</p>
+          <p>Official Community of The Department of <br /> Computer Science & Information Technology, NEDUET</p>
           <NavLink to='/member-registration'
           ><button className='my-btn'>Join Us</button>
           </NavLink>
