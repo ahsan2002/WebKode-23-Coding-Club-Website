b6a965e610e4a258092374b65de12b683a75dc58
Ahsan <omerjee.ahsan@gmail.com>
2023-04-22 01:20:52 +0500

    teams page in progress

---
+++ src/Components/TeamComponent/SideBar/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './SideBar.css'
+
+const Sidebar = () => {
+  return (
+    <>
+        <div className="sidebar">
+                <h2>Our Team</h2>
+        </div>
+    </>
+  )
+}
+
+export default Sidebar
--- src/Components/TeamComponent/index.js
+++ src/Components/TeamComponent/index.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import TeamCard from './TeamCard/TeamCard';
+import Layout from '../../Layout/index';
+import './Team.css';
+import Sidebar from './SideBar/Sidebar';
 
 const index = () => {
     return (
-        <div>index</div>
+        <>
+            <Layout>
+                <div className="regmain">
+                    <h2>Our Team</h2>
+                </div>
+                <div className="team_main" >
+                    <Sidebar />
+                    <div className="cards_main" style={{display:'flex'}}>
+                        <TeamCard />
+                        <TeamCard />
+                        <TeamCard />
+
+                </div>
+                    </div>
+            </Layout>
+        </>
     )
 }
 
