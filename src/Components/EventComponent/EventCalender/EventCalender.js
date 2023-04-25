d7b40e63b7ff97bd0b991e23deebc143104e5ed6
MuhammadDaniyal <daniyalshiekh166@gmail.com>
2023-04-25 18:50:19 +0500

    Event Component Folder structure

---
+++ src/Components/EventComponent/EventCalender/EventCalender.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './EventCalender.css'
+
+const EventCalender = () => {
+    return (
+        <>
+            <div>
+                Calender
+            </div>
+        </>
+    )
+}
+
+export default EventCalender
