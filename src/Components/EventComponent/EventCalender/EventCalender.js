6b193bc5078a7bf20ab8ea07ab40031624868ee4
MuhammadDaniyal <daniyalshiekh166@gmail.com>
2023-04-27 02:00:44 +0500

    add calender

--- src/Components/EventComponent/EventCalender/EventCalender.js
+++ src/Components/EventComponent/EventCalender/EventCalender.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './EventCalender.css'
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 const EventCalender = () => {
+    const [value, onChange] = useState(new Date());
     return (
         <>
             <div>
-                Calender
+                <Calendar onChange={onChange} value={[Date(2023, 5, 4), new Date(2023, 5, 9)]} />
             </div>
         </>
     )
